4fb5a3cbe177a65afe64dfe554032996bf110168
justcoom <vbruner11@mail.ru>
2025-01-13 18:55:22 +0700

    second lesson with objects

---
+++ src/02/02.ts
@@ -0,0 +1,56 @@
+type LocalCityType = {
+    title: string
+    country: string
+}
+
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+
+type TechnologiesType = {
+    id: number
+    title: string
+}
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: TechnologiesType[]
+}
+
+const student: StudentType = {
+    id: 1,
+    name: "Dimych",
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: "surganova 2",
+        city: {
+            title: "Minsk",
+            country: "Belarus",
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTMl",
+        },
+        {
+            id: 2,
+            title: "CSS",
+        },
+        {
+            id: 3,
+            title: "React",
+        }
+    ]
+}
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.country)
+console.log(student.technologies[2].title)
---
+++ src/02/02_02.ts
@@ -0,0 +1,28 @@
+type StreetType = {
+    title: string
+}
+
+type AddressType = {
+    number?: number
+    street: StreetType
+}
+
+type HouseType = {
+    buildedAt: number
+    repaired: boolean
+    address: AddressType
+}
+
+export type governmentBuildingsType = {
+    type: "HOSPITAL" | "FIRE-STATION"
+    budget: number
+    staffCount: number
+    address: AddressType
+}
+
+export type CityType = {
+    title: string
+    houses: Array<HouseType>
+    governmentBuildings: Array<governmentBuildingsType>
+    citizensNumber: number
+}
